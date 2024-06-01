97aefb02bc77c0436dcde3796c3678626ce6a75c
Robbie Hughes <rwhughes91@gmail.com>
2024-06-01 11:58:37 -0700

    added Adapter class

---
+++ app/adapters/__init__.py
@@ -0,0 +1,15 @@
+from app.connections import Connections
+
+from .cache import Cache
+from .cache.redis import RedisCache
+from .publisher import Publisher
+from .publisher.nats import NatsEventPublisher
+
+
+class Adapters:
+    cache: Cache
+    publisher: Publisher
+    
+    def __init__(self):
+        self.cache = RedisCache(Connections.rc)
+        self.publisher = NatsEventPublisher(Connections.nc)
