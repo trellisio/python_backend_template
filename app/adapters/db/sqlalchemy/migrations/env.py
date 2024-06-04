a99b2333ed82e756c3b7f054b22566f3acddfaf5
Robbie Hughes <rwhughes91@gmail.com>
2024-06-04 08:52:38 -0700

    working migration state

--- app/adapters/db/sqlalchemy/migrations/env.py
+++ app/adapters/db/sqlalchemy/migrations/env.py
@@ -3,15 +3,14 @@ import os
 from logging.config import fileConfig
 
 from alembic import context
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import pool
 from sqlalchemy.engine import Connection
-from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.ext.asyncio import async_engine_from_config
 from tables import metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
 config.set_main_option("sqlalchemy.url", os.environ.get("POSTGRES_URL"))
 
 # Interpret the config file for Python logging.
@@ -62,16 +61,16 @@ def do_run_migrations(connection: Connection) -> None:
         context.run_migrations()
 
 
-async def run_migrations_online() -> None:
-    """Run migrations in 'online' mode."""
+async def run_async_migrations() -> None:
+    """In this scenario we need to create an Engine
+    and associate a connection with the context.
 
-    connectable = AsyncEngine(
-        engine_from_config(
-            config.get_section(config.config_ini_section, {}),
-            prefix="sqlalchemy.",
-            poolclass=pool.NullPool,
-            future=True,
-        )
+    """
+
+    connectable = async_engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
     )
 
     async with connectable.connect() as connection:
@@ -80,7 +79,13 @@ async def run_migrations_online() -> None:
     await connectable.dispose()
 
 
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
+
+    asyncio.run(run_async_migrations())
+
+
 if context.is_offline_mode():
     run_migrations_offline()
 else:
-    asyncio.run(run_migrations_online())
+    run_migrations_online()
--- app/adapters/db/sqlalchemy/tables.py
+++ app/adapters/db/sqlalchemy/tables.py
@@ -6,5 +6,5 @@ user = Table(
     "user",
     metadata,
     Column("id", Integer, primary_key=True, autoincrement=True),
-    Column("email", String, nullable=False, unique=True),
+    Column("email", String, nullable=False, unique=True, index=True),
 )
