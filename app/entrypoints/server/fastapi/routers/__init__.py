218c54e889cc723729690490241baad15d2a2049
Robbie Hughes <rwhughes91@gmail.com>
2024-09-29 17:32:09 -0700

    removed versioning from metrics

--- app/entrypoints/server/fastapi/routers/__init__.py
+++ app/entrypoints/server/fastapi/routers/__init__.py
@@ -3,7 +3,12 @@ from fastapi import APIRouter
 from .monitoring import router as monitoring_router
 from .user import user_routes
 
-router = APIRouter(prefix="/v1")
+router = APIRouter()
+v1_router = APIRouter(prefix="/v1")
+router.include_router(v1_router)
 
+# monitoring
 router.include_router(monitoring_router)
-router.include_router(user_routes.router, prefix="/users")
+
+# v1
+v1_router.include_router(user_routes.router, prefix="/users")
